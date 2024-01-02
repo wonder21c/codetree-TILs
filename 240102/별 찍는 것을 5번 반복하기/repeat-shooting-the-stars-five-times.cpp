bca397c2b7af08a50bb46c1311f82b75ec8c8308
손정우 <50861700+wonder21c@users.noreply.github.com>
2024-01-02 10:20:52 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240102/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for(int a = 0; a < 5 ; a ++){
+        for(int b = 0; b < 10 ; b++){
+            cout << "*";
+        }
+        cout << endl;
+
+    }
+    return 0;
+}
